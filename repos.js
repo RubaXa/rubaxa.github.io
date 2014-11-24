204522b52e97dc0eac4f2151d07765c8d3a77973
RubaXa <ibnRubaXa@gmail.com>
2014-11-24 23:43:07 +0300

    + gratipay

--- repos.js
+++ repos.js
@@ -106,6 +106,7 @@
 				'<div style="padding: 40px 0; text-align: center">' +
 				'  <div style="top: -5px" class="fb-like" data-href="' + url +  '" data-layout="button" data-show-faces="true" data-action="' + (el && el.getAttribute('data-fb-action') || 'recommend') + '" ></div>' +
 				'  <div" class="g-plusone" data-size="medium" data-href="' + url + '"></div>' +
+				'  <a href="https://gratipay.com/RubaXa/"><img src="//img.shields.io/gratipay/RubaXa.svg"/></a>' +
 				'</div>'
 			);
 
